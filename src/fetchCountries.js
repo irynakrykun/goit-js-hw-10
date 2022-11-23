b7d7a0f4cd36daed7964b3cad5a78b0aabd168d7
irynakrykun <ira_franyk@ukr.net>
2022-11-23 12:41:39 +0200

    chek

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,18 +1,12 @@
-   function fetchCountries(name = Canada) {
-   return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,flags,capital,languages,population`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
-        })
-};
+function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,flags,capital,languages,population`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
 
-   export { fetchCountries };
-
-
-
-
-
-
-
+export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,69 +1,69 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
-import {fetchCountries} from './fetchCountries';
+import { fetchCountries } from './fetchCountries';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
 
 const inputEl = document.querySelector('#search-box');
 const list = document.querySelector('.country-list');
-const CountryInfo = document.querySelector('.country-info')
+const сountryInfo = document.querySelector('.country-info');
 
-inputEl.addEventListener('input', debounce(onInput,DEBOUNCE_DELAY));
+inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(e) {
-       const nameInput = e.target.value.trim();
-    if (!nameInput) {
-      alert('add Country!');
-        list.innerHTML = '';
-        CountryInfo.innerHTML = '';
-        return
-               
-    };
-    fetchCountries(nameInput).then((data) => {
-       
-        if (data.length > 10) {
-            Notify.info("Too many matches found. Please enter a more specific name.");
-            return;
-        } else if (data.length > 1) { createListCountry(data) }
-          
-        else { createMarkup(data) };
-                              
-    }).catch(err => {
-        Notify.failure("Oops, there is no country with that name.")
-        list.innerHTML = '';
-        CountryInfo.innerHTML = '';
-        
-    }
-         )
-                     
+  const nameInput = e.target.value.trim();
+  if (!nameInput) {
+    Notify.info('add Country!');
+    list.innerHTML = '';
+    сountryInfo.innerHTML = '';
+    return;
+  }
+  fetchCountries(nameInput)
+    .then(data => {
+      if (data.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
+      } else if (data.length > 1) {
+        createListCountry(data);
+      } else {
+        createMarkup(data);
+      }
+    })
+    .catch(err => {
+      Notify.failure('Oops, there is no country with that name.');
+      list.innerHTML = '';
+      сountryInfo.innerHTML = '';
+    });
 }
 
 function createMarkup(countries) {
-    const markup = countries.map(({name, flags, capital, languages, population}) => {
-     return  ` <li>
+  const markup = countries
+    .map(({ name, flags, capital, languages, population }) => {
+      return ` <li>
       <img src=" ${flags.svg}" alt=" ${name}" width = "70" />
       <h2>Country: ${name.official}</h2>
       <h3>Capital: ${capital}</h3>
       <p>Population: ${population}</p>
       <p>Languages: ${Object.values(languages)}</p>
-    </li>`
-    
-    }).join("");
+    </li>`;
+    })
+    .join('');
 
-    CountryInfo.innerHTML = markup;
-    list.innerHTML = '';
-    
-};
+  сountryInfo.innerHTML = markup;
+  list.innerHTML = '';
+}
 function createListCountry(arr) {
-    const listcountry = arr.map(({ flags, name }) => {
-        return ` <li>
+  const listcountry = arr
+    .map(({ flags, name }) => {
+      return ` <li>
       <img src=" ${flags.svg}" alt="flag" width = "70" />
       <h2>Country: ${name.official}</h2>
-          </li>`
-    }).join("");
-    list.innerHTML = listcountry;
-    CountryInfo.innerHTML = '';
-
-};
-
+          </li>`;
+    })
+    .join('');
+  list.innerHTML = listcountry;
+  сountryInfo.innerHTML = '';
+}
